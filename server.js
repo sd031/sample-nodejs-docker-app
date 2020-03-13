61ffff06df50b67de2ffe6d94f4a3128f2ec6be8
Sandip Das <sandip@gryphonconnect.com>
2020-03-13 07:15:19 +0530

    all changes

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 'use strict';
 
+const request = require("request");
 const express = require('express');
 
 // Constants
 const PORT = process.env.PORT || 8080;
 const HOST = process.env.HOST || 'localhost';
+const App2 = process.env.APP_2;
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send(`Hello World`);
+  return res.send(`It's App 1`);
+});
+
+app.get('/call-app-2', (req, res) => {
+var options = { method: 'GET',
+  url: `http://${App2}`,
+  headers: 
+   { 'cache-control': 'no-cache' } };
+
+request(options, function (error, response, body) {
+  if (error) {
+    return res.send(error.message);
+  }
+  return res.send(body);
+});
+
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+console.log(`Running on port :${PORT}`);
